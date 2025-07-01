7422c620f2e91a8df3674da7dcfd01217f8f11cd
viveksingh <viveks98765@gmail.com>
2025-07-01 18:51:49 +0530

    movie app with trending list feature

--- src/appwrite.js
+++ src/appwrite.js
@@ -4,8 +4,10 @@ const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 
+console.log(PROJECT_ID,DATABASE_ID,COLLECTION_ID);
+
 const client = new Client()
-  .setEndpoint('https://fra.cloud.appwrite.io/v1')
+  .setEndpoint("https://nyc.cloud.appwrite.io/v1")
   .setProject(PROJECT_ID)
 
 const database = new Databases(client);
